ca0a7d3a28c346c997ed759dec270cb9f3af754c
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-03-25 12:19:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function verificar() {
     var fano = window.document.getElementById('iano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('Verifique os dados e tente novamente!')
+        window.alert('Verifique os dados e tente novamente!') //se o número digitado do formulário ano for igual a 0 ou maior que o ano atual, o alert vai aparecer com a mensagem digitada acima entre ''
     } else {
         var fsex = document.getElementsByName('sex')
-        var idade = ano - Number(fano.value)
+        var idade = ano - Number(fano.value) //a idade é calculada pela subtração da variável ano que corresponde ao ano atual pelo número do valor da variável fano que corresponde ao ano digitado pelo usuário no formulário
         var gen = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
@@ -43,7 +43,7 @@ function verificar() {
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`  
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`    
         res.appendChild(img)
     }
 }
